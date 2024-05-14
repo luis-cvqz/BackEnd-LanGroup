db264f84b8c3f9b8c0738532e8575d82246fcca6
Luis C <zs21013875@estudiantes.uv.mx>
2024-05-14 13:20:03 -0600

    Modelo y migracion para los archivos de una publicacion

---
+++ migrations/20240514173446-create-archivopublicacion.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('archivopublicacion', {
+      idarchivo: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      publicacionid: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      mime: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      tamanio: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      indb: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      archivo: {
+        type: Sequelize.BLOB,
+        allowNull: true,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('archivopublicacion');
+  }
+};
