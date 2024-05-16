21079b528c22e7bf40731260ece13af80877105f
Luis C <zs21013875@estudiantes.uv.mx>
2024-05-15 19:29:12 -0600

    Modelo & migracion para solicitud

---
+++ migrations/20240516010425-create-solicitud.js
@@ -0,0 +1,56 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('solicitud', {
+      idsolicitud: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      contenido: {
+        type: Sequelize.STRING(1200),
+        allowNull: false
+      },
+      motivo: {
+        type: Sequelize.STRING(1200),
+        allowNull: false
+      },
+      estado: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      constancia: {
+        type: Sequelize.BLOB,
+        allowNull: false
+      },
+      usuarioid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'colaborador',
+          key: 'idusuario'
+        }
+      },
+      idiomaid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'idioma',
+          key: 'ididioma'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('solicitud');
+  }
+};
