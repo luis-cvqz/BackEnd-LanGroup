d1977dbb20231d36fbfd1a2d2b76ea37f42682fb
Luis C <zs21013875@estudiantes.uv.mx>
2024-05-19 01:26:35 -0600

    Controller parcial de publicaciones

---
+++ controllers/publicaciones.controller.js
@@ -0,0 +1,145 @@
+const { publicacion, colaborador, grupo, Sequelize, sequelize } = require('../models')
+const Op = Sequelize.Op
+const crypto = require('crypto')
+
+let self = {}
+
+// GET api/publicaciones
+self.recuperarTodas = async function (req, res) {
+  try {
+    const { idiomaquery, grupoquery } = req.query
+
+    const filtros = {}
+    if (grupoquery) {
+      filtros.grupoid = {
+        [Op.like]: `%${grupo}%`
+      }
+    }
+    if (idiomaquery) {
+      filtros.idioma = idioma
+    }
+
+    const publicaciones = await publicacion.findAll({
+      where: filtros,
+      attributes: [
+        'idpublicacion',
+        'titulo',
+        'descripcion',
+        [Sequelize.col('idioma.nombre', 'idioma')],
+        'fecha',
+        [Sequelize.col('colaborador.nombre', 'instructor')],
+        [Sequelize.col('grupo.nombre', 'grupo')]],
+      include: [
+        { model: idioma, attributes: [] },
+        { model: colaborador, attributes: [] },
+        { model: grupo, attributes: [] },
+        {
+          model: archivomultimedia,
+          as: 'archivos',
+          attributes: ['idarchivo', 'nombre', 'mime', 'tamanio'],
+          throgh: { attributes: [] }
+        },
+      ]
+    })
+
+    return res.status(200).json(publicaciones)
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+// GET API/publicaciones/:id
+self.recuperar = async function (req, res) {
+  try {
+    let id = req.params.idpublicacion
+
+    let data = await publicacion.findByPk(id, {
+      attributes: [
+        'idpublicacion',
+        'titulo',
+        'descripcion',
+        [Sequelize.col('idioma.nombre'), 'idioma'],
+        'fecha',
+        [Sequelize.col('colaborador.nombre'), 'instructor'],
+        [Sequelize.col('grupo.nombre'), 'grupo']],
+      include: [
+        { model: idioma, attributes: [] },
+        { model: colaborador, attributes: [] },
+        { model: grupo, attributes: [] },
+        {
+          model: archivomultimedia,
+          as: 'archivos',
+          attributes: ['idarchivo', 'nombre', 'mime', 'tamanio'],
+          throgh: { attributes: [] }
+        },
+      ]
+    })
+    if (data) 
+      return res.status(200).json(data)
+    else
+      return res.status(404).json({ message: 'No se encontró la publicación' })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+// POST api/publicaciones
+self.crear = async function (req, res) {
+  try {
+    let data = await publicacion.create({
+      idpublicacion: crypto.randomUUID(),
+      titulo: req.body.titulo,
+      descripcion: req.body.descripcion,
+      fecha: new Date(),
+      usuarioid: req.body.usuarioid,
+      grupoclave: req.body.grupoid,
+    })
+    return res.status(201).json(data)
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+// PUT api/publicaciones/:id
+// TODO: Implementar el método para actualizar una publicación
+
+self.eliminar = async function (req, res) {
+  try {
+    let id = req.params.idpublicacion
+    let data = await publicacion.findByPk(id)
+
+    if (!data)
+      return res.status(404).send()
+
+    data = await publicacion.destroy({ where: { idpublicacion: id } })
+    
+    if (data === 1)
+      return res.status(204).send()
+    else
+      return res.status(404).send()
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+// POST api/publicaciones/:idpublicacion/archivos
+self.asignaArchivo = async function (req, res) {
+  try {
+    let archivo = await archivomultimedia.findByPk(req.body.idarchivo)
+    if (!archivo) return res.status(404).send()
+    
+    let publicacion = await publicacion.findByPk(req.params.idpublicacion)
+    if (!publicacion) return res.status(404).send()
+
+    await data.addArchivos(archivo)
+    return res.status(204).send()
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+// DELETE api/publicaciones/:idpublicacion/archivos/:idarchivo
+// TODO: Implementar el método para desasignar un archivo de una publicación
+
+module.exports = self
