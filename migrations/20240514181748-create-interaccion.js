48098fba9a0db0107f1ce8dc9d5a368477ed3178
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-05-21 19:35:43 -0600

    Nombre llave primara

--- migrations/20240514181748-create-interaccion.js
+++ migrations/20240514181748-create-interaccion.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('interaccion', {
-      idinteraccion: {
+      id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID
--- models/publicacion.js
+++ models/publicacion.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   publicacion.init(
     {
-      idpublicacion: {
+      id: {
         type: DataTypes.STRING,
         primaryKey: true,
       },
