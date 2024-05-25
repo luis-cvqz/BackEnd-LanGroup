47589f4a2c54bad3091a7da8b7ae173d9cd57c65
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-05-25 17:00:28 -0600

    Agregar rutas a index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
 const app = express()
 
 dotenv.config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// cors
+var corsOptions = {
+    origin: ["http://localhost:3001", "http://localhost:8080"],
+    methods: "GET, PUT, POST, DELETE"
+}
+app.use(cors(corsOptions))
+
+app.use("/api/publicaciones", require("./routes/publicaciones.routes"))
+app.use("/api/archivosmultimedia", require("./routes/archivmosmultimedia.routes"))
+app.use("/api/idiomas", require("./routes/idiomas.routes"))
+app.get('*', (req,res) => { res.status(404).send() })
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Backend de LanGroup escuchando en el puerto: ${process.env.SERVER_PORT}`)
