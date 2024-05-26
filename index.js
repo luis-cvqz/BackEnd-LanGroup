25cbed2ad6453d73f37cdc4fc739ea0e30059df8
Narayani <fa.narayani@gmail.com>
2024-05-26 13:13:03 -0600

    controlador de colaborador y autorización

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const cors = require("cors");
 const dotenv = require('dotenv')
 const app = express()
 
 dotenv.config()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Cors
+var corsOptions = {
+    origin: ["http://localhost:3001", "http://localhost:8080"],
+    methods: "GET,PUT,POST,DELETE",
+};
+
+app.use(cors(corsOptions));
+
+// Rutas
+app.use("/api/colaboradores", require('./routes/colaboradores.routes'))
+app.use("/api/auth", require('./routes/auth.routes'))
+app.get('*', (req, res) => { res.status(404).send() });
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Backend de LanGroup escuchando en el puerto: ${process.env.SERVER_PORT}`)
