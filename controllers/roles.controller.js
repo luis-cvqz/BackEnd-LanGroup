78df239db9fa9a8525d1fa2be963e414d95e0ea6
Froylan-Aarkan <77468347+Froylan-Aarkan@users.noreply.github.com>
2024-05-31 12:47:20 -0600

    Correccion roles.controller

--- controllers/roles.controller.js
+++ controllers/roles.controller.js
@@ -2,12 +2,12 @@ const { where } = require('sequelize');
 const { rol } = require('../models')
 let self = {}
 
-// GET /api/roles/{nombre}
+// GET /api/roles/{id}
 self.recuperar = async function (req, res){
     try{
-        let nombre = req.params.nombre;
+        let id = req.params.id;
         
-        let data = await rol.find(nombre, {
+        let data = await rol.findByPk(id, {
             attributes: [ 'id', 'nombre']
         })
 
@@ -16,6 +16,24 @@ self.recuperar = async function (req, res){
         else
             return res.status(404).json({ message: 'No se encontró el rol' })
     } catch (error){
+        return res.status(500).json(error)
+    }
+}
+
+// GET /api/roles/
+self.recuperarTodos = async function (req, res){
+    try{
+        let data = await rol.findAll({
+            attributes: ['id', 'nombre'],
+            subQuery: false
+        })
+
+        if(data)
+            return res.status(200).json(data)
+        else
+        return res.status(404).send()
+
+    }catch(error){
         return res.status(500).json(error)
     }
 }
--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const roles = require('../controllers/roles.controller')
 
-// GET: api/roles/{nombre}
-router.get('/:nombre', roles.recuperar);
+// GET: api/roles/{id}
+router.get('/:id', roles.recuperar);
+
+// GET: api/roles/
+router.get('/', roles.recuperarTodos);
 
 module.exports = router
