484f4c74aa9d3d4b80f6b8b6009aabe125167775
Luis C <zs21013875@estudiantes.uv.mx>
2024-06-01 18:56:50 -0600

    Agregar logger al middleware de autenticacion

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const jwtSecret = process.env.JWT_SECRET
 const ClaimTypes = require('../config/claimtypes')
+const logger = require('../logger/logger')
 const { GeneraToken } = require('../services/jwttoken.service')
 
 const Authorize = (rol) => {
