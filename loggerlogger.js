df112fbb1a7b8b62067c7dec61fbdfa073b1baa6
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-06-05 20:56:03 -0600

    Actualizar swagger

--- loggerlogger.js
+++
@@ -1,2 +0,0 @@
-2024-05-28T16:32:26.508Z [error]: Grupo con id eaa43b31-1b15-11ef-8985-0a002700001 no encontrado.
-2024-05-28T16:33:15.552Z [error]: Grupo con id eaa43b31-1b15-11ef-8985-0a002700001 no encontrado.
