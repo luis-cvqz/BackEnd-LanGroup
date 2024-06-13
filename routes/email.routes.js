9af771ca59ff241bf35bbeeab79a15cbb725469b
Luis C <zs21013875@estudiantes.uv.mx>
2024-06-12 21:04:35 -0600

    Roles de autorizacion como enum

--- routes/email.routes.js
+++ routes/email.routes.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 const emailController = require("../controllers/email.controller"); // Asegúrate de que la ruta es correcta
 const Authorize = require("../middlewares/auth.middleware");
+const Roles = require("../enums/roles.enum");
 
 // Definir la ruta para enviar correos
-router.post("/correo", Authorize("Administrador,Instructor,Aprendiz"), emailController.enviarCorreo);
+router.post("/correo", Authorize(`${Roles.ADMINISTRADOR},${Roles.INSTRUCTOR},${Roles.APRENDIZ}`), emailController.enviarCorreo);
 
 module.exports = router;
--- routes/interacciones.routes.js
+++ routes/interacciones.routes.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 const interacciones = require('../controllers/interacciones.controller')
 const Authorize = require('../middlewares/auth.middleware')
-const AuthorizeActualizacion = require("../middlewares/authActualizar.middleware");
+const AuthorizeActualizacion = require("../middlewares/authActualizar.middleware")
+const Roles = require("../enums/roles.enum")
 
 // GET /api/interacciones/{publicacionid}
-router.get("/:publicacion", Authorize('Administrador,Instructor,Aprendiz'), interacciones.recuperarTodos);
+router.get("/:publicacion", Authorize(`${Roles.ADMINISTRADOR},${Roles.INSTRUCTOR},${Roles.APRENDIZ}`), interacciones.recuperarTodos);
 
 // POST /api/interacciones
-router.post("/", Authorize('Administrador,Instructor,Aprendiz'), interacciones.crear);
+router.post("/", Authorize(`${Roles.ADMINISTRADOR},${Roles.INSTRUCTOR},${Roles.APRENDIZ}`), interacciones.crear);
 
 // PUT /api/interacciones/{interaccionid}
-router.put("/:interaccion", Authorize('Administrador,Instructor,Aprendiz'), AuthorizeActualizacion, interacciones.actualizar);
+router.put("/:interaccion", Authorize(`${Roles.ADMINISTRADOR},${Roles.INSTRUCTOR},${Roles.APRENDIZ}`), AuthorizeActualizacion, interacciones.actualizar);
 
 // DELETE /api/interacciones/{interaccionid}
-router.delete("/:interaccion", Authorize('Administrador,Instructor,Aprendiz'), interacciones.eliminar);
+router.delete("/:interaccion", Authorize(`${Roles.ADMINISTRADOR},${Roles.INSTRUCTOR},${Roles.APRENDIZ}`), interacciones.eliminar);
 
 module.exports = router
